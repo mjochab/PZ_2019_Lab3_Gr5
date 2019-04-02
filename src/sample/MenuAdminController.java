02db44131cc5019342a69ee000ec28f3bcf83a8f
Łukasz Kowalski <kofcio0@vp.pl>
2019-04-02 06:28:13 +0200

    Okna zarządzania admina

--- src/sample/MenuAdminController.java
+++ src/sample/MenuAdminController.java
@@ -4,11 +4,10 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.AnchorPane;
+
 import java.io.IOException;
 
-
 public class MenuAdminController {
-
     @FXML
     private AnchorPane adminPane;
 
@@ -18,5 +17,24 @@ public class MenuAdminController {
         adminPane.getChildren().setAll(pane);
     }
 
+    public void menuAdmin(ActionEvent event) throws IOException {
+        AnchorPane pane = FXMLLoader.load(getClass().getResource("menuAdmin.fxml"));
+        adminPane.getChildren().setAll(pane);
+    }
+
+    public void zarzadzajUzytkownikamiAdmin(ActionEvent event) throws IOException {
+        AnchorPane pane = FXMLLoader.load(getClass().getResource("zarzadzajUzytkownikamiAdmin.fxml"));
+        adminPane.getChildren().setAll(pane);
+    }
+
+    public void zarzadzajPojazdamiAdmin(ActionEvent event) throws IOException {
+        AnchorPane pane = FXMLLoader.load(getClass().getResource("zarzadzajPojazdamiAdmin.fxml"));
+        adminPane.getChildren().setAll(pane);
+    }
+
+    public void zarzadzajWypozyczeniamiAdmin(ActionEvent event) throws IOException {
+        AnchorPane pane = FXMLLoader.load(getClass().getResource("zarzadzajWypozyczeniamiAdmin.fxml"));
+        adminPane.getChildren().setAll(pane);
+    }
 
 }
