7038038767f91fd49b66b4b9d52ecd4e8a82a1dc
Łukasz Kowalski <kofcio0@vp.pl>
2019-04-09 08:58:13 +0200

    Poprawki, implemenation of hibernate

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("Fxml/login.fxml"));
         primaryStage.setTitle("Wypozyczalnia Samochodow");
-        primaryStage.setScene(new Scene(root, 700, 400));
+        primaryStage.setScene(new Scene(root, 800, 400));
         primaryStage.show();
     }
 
