a8a8d035e5d4627e0080bb1e3979f394c6ac7b96
Pifciu <filipkonior1@gmail.com>
2019-04-15 14:23:01 +0200

    testowe połączenie z bazą

--- src/sample/Controllers/LoginController.java
+++ src/sample/Controllers/LoginController.java
@@ -2,13 +2,19 @@ package sample.Controllers;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ResourceBundle;
 
+import com.mysql.cj.protocol.Resultset;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.AnchorPane;
+import org.hibernate.jdbc.Expectation;
 
 public class LoginController implements Initializable {
 
@@ -38,6 +44,19 @@ public class LoginController implements Initializable {
     public void printHello(ActionEvent event){
         System.out.println("Nie ma jeszcze bazy danych!");
 
+    try {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1/projekt_zespolowe?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "" );
+        Statement statement = con.createStatement();
+        ResultSet rs = statement.executeQuery("select * from klient");
+        while(rs.next())
+        {
+            System.out.println(rs.getString(4));
+        }
 
+    }catch (Exception e)
+    {
+        System.out.println(e);
+    };
     }
 }
