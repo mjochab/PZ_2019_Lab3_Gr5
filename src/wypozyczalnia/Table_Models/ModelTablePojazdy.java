7cc8ff5e8c4d22ad3c6db8b36dd31602ad7d0c6e
Tommy118 <tommyhalfniger@hotmail.com>
2019-05-20 09:51:00 +0200

    Zarządzanie pojazdem - finalne poprawki

--- src/wypozyczalnia/Table_Models/ModelTablePojazdy.java
+++ src/wypozyczalnia/Table_Models/ModelTablePojazdy.java
@@ -26,6 +26,14 @@ public class ModelTablePojazdy {
         this.rodzaj = rodzaj;
     }
 
+    public String getRocznik() {
+        return rocznik;
+    }
+
+    public void setRocznik(String rocznik) {
+        this.rocznik = rocznik;
+    }
+
     public String getPaliwo() {
         return paliwo;
     }
@@ -50,15 +58,16 @@ public class ModelTablePojazdy {
         this.cena = cena;
     }
 
-    public ModelTablePojazdy(String marka, String model, String rodzaj, String paliwo, String przebieg, String cena) {
+    public ModelTablePojazdy(String marka, String model, String rodzaj, String rocznik, String paliwo, String przebieg, String cena) {
         this.marka = marka;
         this.model = model;
         this.rodzaj = rodzaj;
+        this.rocznik = rocznik;
         this.paliwo = paliwo;
         this.przebieg = przebieg;
         this.cena = cena;
     }
 
-    String marka,model,rodzaj,paliwo,przebieg,cena;
+    String marka,model,rodzaj,rocznik,paliwo,przebieg,cena;
 
 }
