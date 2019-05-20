d4910e2b6fc2ef4e7f8ed1c15ac5cd39f562cbce
Łukasz Kowalski <kofcio0@vp.pl>
2019-05-20 20:49:02 +0200

    Udostepnij samochód pracownik - do poprawek

--- src/wypozyczalnia/Controllers/WypozyczeniePracownikController.java
+++ src/wypozyczalnia/Controllers/WypozyczeniePracownikController.java
@@ -1,15 +1,62 @@
 package wypozyczalnia.Controllers;
 
+        import javafx.collections.FXCollections;
+        import javafx.collections.ObservableList;
         import javafx.event.ActionEvent;
         import javafx.fxml.FXML;
         import javafx.fxml.FXMLLoader;
+        import javafx.fxml.Initializable;
+        import javafx.scene.control.TableColumn;
+        import javafx.scene.control.TableView;
+        import javafx.scene.control.TextField;
+        import javafx.scene.control.cell.PropertyValueFactory;
         import javafx.scene.layout.AnchorPane;
+        import wypozyczalnia.DBConnector;
 
         import java.io.IOException;
+        import java.net.URL;
+        import java.sql.Connection;
+        import java.sql.ResultSet;
+        import java.sql.SQLException;
+        import java.util.ResourceBundle;
+        import java.util.logging.Level;
+        import java.util.logging.Logger;
 
-public class WypozyczeniePracownikController {
+public class WypozyczeniePracownikController implements Initializable {
     @FXML
     private AnchorPane pracownikPane;
+
+    @FXML
+    private TableView<ModelTablePojazdy> tabelka_pojazdy;
+    @FXML
+    private TableColumn<ModelTablePojazdy, String> col_marka;
+    @FXML
+    private TableColumn<ModelTablePojazdy, String> col_model;
+    @FXML
+    private TableColumn<ModelTablePojazdy, String> col_rodzaj;
+    @FXML
+    private TableColumn<ModelTablePojazdy, String> col_rocznik;
+    @FXML
+    private TableColumn<ModelTablePojazdy, String> col_paliwo;
+    @FXML
+    private TableColumn<ModelTablePojazdy, String> col_przebieg;
+    @FXML
+    private TableColumn<ModelTablePojazdy, Double> col_cena;
+    @FXML
+    private TableColumn<ModelTablePojazdy, String> col_dostep;
+
+
+    @FXML private TextField autoMarka;
+    @FXML private TextField autoModel;
+    @FXML private TextField autoRodzaj;
+    @FXML private TextField autoRocznik;
+    @FXML private TextField autoPaliwo;
+    @FXML private TextField autoPrzebieg;
+    @FXML private TextField autoCena;
+    @FXML private TextField autoDostep;
+
+    ObservableList<ModelTablePojazdy> oblist1 = FXCollections.observableArrayList();
+
 
 
 
@@ -30,6 +77,40 @@ public class WypozyczeniePracownikController {
     public void podWynP(ActionEvent event) throws IOException {
         AnchorPane pane = FXMLLoader.load(getClass().getResource("../fxml/podWynP.fxml"));
         pracownikPane.getChildren().setAll(pane);
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        try {
+            Connection con = DBConnector.getConnection();
+
+            ResultSet rs = con.createStatement().executeQuery("SELECT * FROM `samochod`");
+
+            while (rs.next()) {
+                //oblist1.add(new ModelTablePojazdy(rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6), rs.getString(7)));
+                oblist1.add(new ModelTablePojazdy(rs.getString(2),rs.getString(3),rs.getString(4),rs.getString(5),rs.getString(6),rs.getString(7),rs.getString(8),rs.getString(9)));
+            }
+
+
+        } catch (SQLException ex) {
+            Logger.getLogger(zarzadzajUzytkownikamiAdminController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+
+        col_marka.setCellValueFactory(new PropertyValueFactory<>("marka"));
+        col_model.setCellValueFactory(new PropertyValueFactory<>("model"));
+        col_rodzaj.setCellValueFactory(new PropertyValueFactory<>("rodzaj"));
+        col_rocznik.setCellValueFactory(new PropertyValueFactory<>("rocznik"));
+        col_paliwo.setCellValueFactory(new PropertyValueFactory<>("paliwo"));
+        col_przebieg.setCellValueFactory(new PropertyValueFactory<>("przebieg"));
+        col_cena.setCellValueFactory(new PropertyValueFactory<>("cena"));
+        col_dostep.setCellValueFactory(new PropertyValueFactory<>("dostepnosc"));
+
+
+        tabelka_pojazdy.setItems(oblist1);
+
+
     }
 
 
