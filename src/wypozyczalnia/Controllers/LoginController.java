d1df888238b0f25475cfe5f89f4530d03f527e00
Tommy118 <tommyhalfniger@hotmail.com>
2019-05-27 23:17:05 +0200

    Dodanie przykładowych insertów

--- src/wypozyczalnia/Controllers/LoginController.java
+++ src/wypozyczalnia/Controllers/LoginController.java
@@ -7,10 +7,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import wypozyczalnia.UserSession;
@@ -34,6 +31,9 @@ public class LoginController implements Initializable {
     @FXML
     private AnchorPane rootPane;
 
+    @FXML
+    private Button dodajInserty;
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -55,138 +55,6 @@ public class LoginController implements Initializable {
         rootPane.getChildren().setAll(pane);
     }
 
- /*   public void printHello(ActionEvent event){
-
-    try {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1/projekt_zespolowe?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "" );
-        PreparedStatement stmt = con.prepareStatement("INSERT INTO klient VALUES(?,?,?,?,?,?,?,?)");
-        stmt.setInt(1,1);
-        stmt.setString(2,"adik");
-        stmt.setString(3,"adik");
-        stmt.setString(4,"Jan");
-        stmt.setString(5,"Kowalski");
-        stmt.setString(6,"1999-01-01");
-        stmt.setString(7,"Sanok");
-        stmt.setFloat(8,12345678912F);
-        stmt.executeUpdate();
-
-
-        PreparedStatement stmt2 = con.prepareStatement("INSERT INTO pracownik VALUES(?,?,?,?,?,?,?)");
-        stmt2.setInt(1,1);
-        stmt2.setInt(2,1);
-        stmt2.setString(3,"adik");
-        stmt2.setString(4,"adik");
-        stmt2.setString(5,"Jan");
-        stmt2.setString(6,"Kowalski");
-        stmt2.setFloat(7, 12345678912F);
-        stmt2.executeUpdate();
-
-
-        PreparedStatement stmt3 = con.prepareStatement("INSERT INTO administrator VALUES(?,?,?,?,?,?,?)");
-        stmt3.setInt(1,1);
-        stmt3.setInt(2,1);
-        stmt3.setString(3,"adik");
-        stmt3.setString(4,"adik");
-        stmt3.setString(5,"Jan");
-        stmt3.setString(6,"Kowalski");
-        stmt3.setFloat(7,12345678912f);
-        stmt3.executeUpdate();
-
-        PreparedStatement stmt4 = con.prepareStatement("INSERT INTO samochod VALUES(?,?,?,?,?,?,?)");
-        stmt4.setInt(1, 1);
-        stmt4.setString(2, "Renault");
-        stmt4.setString(3,"Laguna");
-        stmt4.setString(4,"Sedan");
-        stmt4.setString(5,"Diesel");
-        stmt4.setString(6, "130000");
-        stmt4.setString(7,"1500");
-        stmt4.executeUpdate();
-
-        PreparedStatement stmt5 = con.prepareStatement("INSERT INTO samochod VALUES(?,?,?,?,?,?,?)");
-        stmt5.setInt(1, 2);
-        stmt5.setString(2, "BMW");
-        stmt5.setString(3,"Seria 1");
-        stmt5.setString(4,"BMX");
-        stmt5.setString(5,"Benzyna");
-        stmt5.setString(6, "25000");
-        stmt5.setString(7,"500");
-        stmt5.executeUpdate();
-
-        PreparedStatement stmt6 = con.prepareStatement("INSERT INTO samochod VALUES(?,?,?,?,?,?,?)");
-        stmt6.setInt(1, 3);
-        stmt6.setString(2, "Pegueot");
-        stmt6.setString(3,"405");
-        stmt6.setString(4,"Pick Up");
-        stmt6.setString(5,"Diesel");
-        stmt6.setString(6, "7000");
-        stmt6.setString(7,"2000");
-        stmt6.executeUpdate();
-
-        PreparedStatement stmt7 = con.prepareStatement("INSERT INTO samochod VALUES(?,?,?,?,?,?,?)");
-        stmt7.setInt(1, 4);
-        stmt7.setString(2, "Jaguar");
-        stmt7.setString(3,"Huracane");
-        stmt7.setString(4,"Limousine");
-        stmt7.setString(5,"Benzyna");
-        stmt7.setString(6, "13000");
-        stmt7.setString(7,"21000");
-        stmt7.executeUpdate();
-
-        PreparedStatement stmt8 = con.prepareStatement("INSERT INTO samochod VALUES(?,?,?,?,?,?,?)");
-        stmt8.setInt(1, 5);
-        stmt8.setString(2, "Renault");
-        stmt8.setString(3,"Laguna");
-        stmt8.setString(4,"Sedan");
-        stmt8.setString(5,"Diesel");
-        stmt8.setString(6, "130000");
-        stmt8.setString(7,"1500");
-        stmt8.executeUpdate();
-
-        PreparedStatement stmt9 = con.prepareStatement("INSERT INTO samochod VALUES(?,?,?,?,?,?,?)");
-        stmt9.setInt(1, 6);
-        stmt9.setString(2, "BMW");
-        stmt9.setString(3,"Seria 1");
-        stmt9.setString(4,"BMX");
-        stmt9.setString(5,"Benzyna");
-        stmt9.setString(6, "25000");
-        stmt9.setString(7,"500");
-        stmt9.executeUpdate();
-
-        PreparedStatement stmt10 = con.prepareStatement("INSERT INTO samochod VALUES(?,?,?,?,?,?,?)");
-        stmt10.setInt(1, 7);
-        stmt10.setString(2, "Pegueot");
-        stmt10.setString(3,"405");
-        stmt10.setString(4,"Pick Up");
-        stmt10.setString(5,"Diesel");
-        stmt10.setString(6, "7000");
-        stmt10.setString(7,"2000");
-        stmt10.executeUpdate();
-
-        PreparedStatement stmt11 = con.prepareStatement("INSERT INTO samochod VALUES(?,?,?,?,?,?,?)");
-        stmt11.setInt(1, 8);
-        stmt11.setString(2, "Jaguar");
-        stmt11.setString(3,"Huracane");
-        stmt11.setString(4,"Limousine");
-        stmt11.setString(5,"Benzyna");
-        stmt11.setString(6, "13000");
-        stmt11.setString(7,"21000");
-        stmt11.executeUpdate();
-
-        ResultSet rs = stmt2.executeQuery("select * from administrator");
-
-        while(rs.next())
-        {
-            System.out.println("Pomyslnie dodano rekordy");
-        }
-
-
-    }catch (Exception e)
-    {
-        System.out.println(e);
-    }
-
-    }*/
 
     public void logIn(ActionEvent actionEvent) {
 
@@ -196,7 +64,7 @@ public class LoginController implements Initializable {
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            con = DriverManager.getConnection("jdbc:mysql://127.0.0.1/projekt_zespolowe?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "" );
+            con = DriverManager.getConnection("jdbc:mysql://127.0.0.1/projekt_zespolowe?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "");
 
             ps = con.prepareStatement("SELECT * FROM user WHERE login = ? AND haslo = ?");
 
@@ -205,7 +73,7 @@ public class LoginController implements Initializable {
             rs = ps.executeQuery();
 
             if (rs.next()) {
-                if(rs.getString("rodzaj").equals("klient")) {
+                if (rs.getString("rodzaj").equals("klient")) {
                     UserSession.getInstance(rs.getString("login"), rs.getString("haslo"), rs.getString("rodzaj"), rs.getInt("user_id"));
                     FXMLLoader Loader = new FXMLLoader();
                     Loader.setLocation(getClass().getResource("../fxml/menuKlient.fxml"));
@@ -225,7 +93,7 @@ public class LoginController implements Initializable {
                     Stage stage = new Stage();
                     stage.setScene(new Scene(p));
                     stage.show();
-                }else if(rs.getString("rodzaj").equals("admin")){
+                } else if (rs.getString("rodzaj").equals("admin")) {
                     UserSession.getInstance(rs.getString("login"), rs.getString("haslo"), rs.getString("rodzaj"), rs.getInt("user_id"));
                     FXMLLoader Loader = new FXMLLoader();
                     Loader.setLocation(getClass().getResource("../fxml/menuAdmin.fxml"));
@@ -245,7 +113,7 @@ public class LoginController implements Initializable {
                     Stage stage = new Stage();
                     stage.setScene(new Scene(p));
                     stage.show();
-                }else if(rs.getString("rodzaj").equals("worker")) {
+                } else if (rs.getString("rodzaj").equals("worker")) {
                     UserSession.getInstance(rs.getString("login"), rs.getString("haslo"), rs.getString("rodzaj"), rs.getInt("user_id"));
                     FXMLLoader Loader = new FXMLLoader();
                     Loader.setLocation(getClass().getResource("../fxml/menuPracownik.fxml"));
@@ -277,9 +145,149 @@ public class LoginController implements Initializable {
             }
 
 
-
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    public void dodajInserty(ActionEvent event) {
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1/projekt_zespolowe?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "");
+
+            PreparedStatement stmt = con.prepareStatement("INSERT INTO samochod VALUES(?,?,?,?,?,?,?,?,?,?)");
+
+            stmt.setInt(1, 0);
+            stmt.setString(2, "Ferrari");
+            stmt.setString(3, "Enzo");
+            stmt.setString(4, "Coupe");
+            stmt.setInt(5, 2008);
+            stmt.setString(6, "Benzyna");
+            stmt.setInt(7, 25000);
+            stmt.setInt(8, 5979);
+            stmt.setString(9, "TAK");
+            stmt.setInt(10, 51);
+            stmt.executeUpdate();
+
+            PreparedStatement stmt2 = con.prepareStatement("INSERT INTO samochod VALUES(?,?,?,?,?,?,?,?,?,?)");
+
+            stmt2.setInt(1, 0);
+            stmt2.setString(2, "Lamborghini");
+            stmt2.setString(3, "Hurracan");
+            stmt2.setString(4, "Coupe");
+            stmt2.setInt(5, 2016);
+            stmt2.setString(6, "Benzyna");
+            stmt2.setInt(7, 34532);
+            stmt2.setInt(8, 5979);
+            stmt2.setString(9, "TAK");
+            stmt2.setInt(10, 51);
+            stmt2.executeUpdate();
+
+            PreparedStatement stmt3 = con.prepareStatement("INSERT INTO samochod VALUES(?,?,?,?,?,?,?,?,?,?)");
+
+            stmt3.setInt(1, 0);
+            stmt3.setString(2, "Lada");
+            stmt3.setString(3, "Samara");
+            stmt3.setString(4, "Hatchback");
+            stmt3.setInt(5, 1995);
+            stmt3.setString(6, "Benzyna");
+            stmt3.setInt(7, 350500);
+            stmt3.setInt(8, 59);
+            stmt3.setString(9, "TAK");
+            stmt3.setInt(10, 51);
+            stmt3.executeUpdate();
+
+            PreparedStatement stmt4 = con.prepareStatement("INSERT INTO samochod VALUES(?,?,?,?,?,?,?,?,?,?)");
+
+            stmt4.setInt(1, 0);
+            stmt4.setString(2, "Pagani");
+            stmt4.setString(3, "Zonda");
+            stmt4.setString(4, "Coupe");
+            stmt4.setInt(5, 2008);
+            stmt4.setString(6, "Benzyna");
+            stmt4.setInt(7, 5000);
+            stmt4.setInt(8, 9755);
+            stmt4.setString(9, "TAK");
+            stmt4.setInt(10, 51);
+            stmt4.executeUpdate();
+
+            PreparedStatement stmt5 = con.prepareStatement("INSERT INTO samochod VALUES(?,?,?,?,?,?,?,?,?,?)");
+
+            stmt5.setInt(1, 0);
+            stmt5.setString(2, "Honda");
+            stmt5.setString(3, "CRX");
+            stmt5.setString(4, "Hatchback");
+            stmt5.setInt(5, 1996);
+            stmt5.setString(6, "Benzyna");
+            stmt5.setInt(7, 255600);
+            stmt5.setInt(8, 125);
+            stmt5.setString(9, "TAK");
+            stmt5.setInt(10, 51);
+            stmt5.executeUpdate();
+
+            PreparedStatement stmt6 = con.prepareStatement("INSERT INTO user VALUES(?,?,?,?,?,?,?,?,?,?,?)");
+
+            stmt6.setInt(1, 0);
+            stmt6.setString(2, "pracownik1000");
+            stmt6.setString(3, "pracownik1000");
+            stmt6.setString(4, "Ryszard");
+            stmt6.setString(5, "Nowak");
+            stmt6.setString(6, "1991-05-20");
+            stmt6.setString(7, "Warszawa");
+            stmt6.setInt(8, 773050222);
+            stmt6.setString(9, "pracownik1000@email.com");
+            stmt6.setString(10, "91052012345");
+            stmt6.setString(11, "worker");
+            stmt6.executeUpdate();
+
+            PreparedStatement stmt7 = con.prepareStatement("INSERT INTO user VALUES(?,?,?,?,?,?,?,?,?,?,?)");
+
+            stmt7.setInt(1, 0);
+            stmt7.setString(2, "klient1000");
+            stmt7.setString(3, "klient1000");
+            stmt7.setString(4, "Piotr");
+            stmt7.setString(5, "Pawlikowski");
+            stmt7.setString(6, "1992-03-26");
+            stmt7.setString(7, "Warszawa");
+            stmt7.setInt(8, 773050333);
+            stmt7.setString(9, "klient1000@email.com");
+            stmt7.setString(10, "92032612345");
+            stmt7.setString(11, "worker");
+            stmt7.executeUpdate();
+
+            PreparedStatement stmt8 = con.prepareStatement("INSERT INTO user VALUES(?,?,?,?,?,?,?,?,?,?,?)");
+
+            stmt8.setInt(1, 0);
+            stmt8.setString(2, "admin1000");
+            stmt8.setString(3, "admin1000");
+            stmt8.setString(4, "Piotr");
+            stmt8.setString(5, "Pawlikowski");
+            stmt8.setString(6, "1984-08-11");
+            stmt8.setString(7, "Warszawa");
+            stmt8.setInt(8, 773050444);
+            stmt8.setString(9, "admin1000@email.com");
+            stmt8.setString(10, "84081112345");
+            stmt8.setString(11, "worker");
+            stmt8.executeUpdate();
+
+
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Informacja");
+            alert.setHeaderText(null);
+            alert.setContentText("Pomyślnie dodano rekordy do bazy!");
+            alert.showAndWait();
+
+            int zmienna = 1;
+            zmienna++;
+            if (zmienna > 1) {
+                dodajInserty.setVisible(false);
+            }
+
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        ;
     }
 }
