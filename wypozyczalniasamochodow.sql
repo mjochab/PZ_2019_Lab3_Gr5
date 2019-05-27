b7159383c0709a2f0ef6b24769540103f2fdcce1
Tommy118 <tommyhalfniger@hotmail.com>
2019-05-28 01:51:53 +0200

    dodanie rekordów do bazy

--- wypozyczalniasamochodow.sql
+++ wypozyczalniasamochodow.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 27 Maj 2019, 22:01
+-- Czas generowania: 28 Maj 2019, 01:51
 -- Wersja serwera: 10.1.38-MariaDB
 -- Wersja PHP: 7.3.3
 
@@ -51,7 +51,7 @@ INSERT INTO `samochod` (`samochod_id`, `marka`, `model`, `rodzaj`, `rocznik`, `p
 (3, 'Pegueot', '206', 'Hatchback', 2006, 'Diesel', 150000, 250, 'NIE', 48),
 (4, 'Renault', 'Laguna', 'Kombi', 1999, 'Gaz', 250000, 135, 'TAK', 91),
 (5, 'Renault', 'Megane', 'Kombi', 2009, 'Diesel', 250000, 125, 'TAK', 93),
-(6, 'Renault', 'Clio', 'Hatchback', 2006, 'Gaz', 150000, 1500, 'NIE', 93),
+(6, 'Renualt', 'Clio', 'Hatchback', 2006, 'Gaz', 150000, 1500, 'NIE', 93),
 (7, 'Citroen', 'C4', 'Hatchback', 2006, 'Benzyna', 150000, 150, 'NIE', 92),
 (8, 'Audi', 'TT', 'Coupe', 2005, 'Benzyna', 58000, 300, 'NIE', 93),
 (9, 'Mazda', '6', 'Sedan', 2015, 'Benzyna', 15000, 500, 'TAK', 43),
@@ -67,7 +67,22 @@ INSERT INTO `samochod` (`samochod_id`, `marka`, `model`, `rodzaj`, `rocznik`, `p
 (19, 'Fiat', 'Tipo', 'Sedan', 2018, 'Gaz', 58400, 500, 'TAK', 38),
 (20, 'Fiat', 'Panda', 'Hatchback', 2007, 'Diesel', 175000, 140, 'TAK', 93),
 (21, 'Ford', 'Focus', 'Hatchback', 2015, 'Diesel', 100000, 730, 'TAK', 45),
-(22, 'Chevrolet', 'Camaro', 'Coupe', 2017, 'Benzyna', 35500, 1800, 'TAK', 93);
+(22, 'Chevrolet', 'Camaro', 'Coupe', 2017, 'Benzyna', 35500, 1800, 'TAK', 93),
+(23, 'Ferrari', 'Enzo', 'Coupe', 2008, 'Benzyna', 25000, 5979, 'TAK', 51),
+(24, 'Lamborghini', 'Hurracan', 'Coupe', 2016, 'Benzyna', 34532, 5979, 'TAK', 51),
+(25, 'Lada', 'Samara', 'Hatchback', 1995, 'Benzyna', 350500, 59, 'TAK', 51),
+(26, 'Pagani', 'Zonda', 'Coupe', 2008, 'Benzyna', 5000, 9755, 'TAK', 51),
+(27, 'Honda', 'CRX', 'Hatchback', 1996, 'Benzyna', 255600, 125, 'TAK', 51),
+(28, 'Ferrari', 'Enzo', 'Coupe', 2008, 'Benzyna', 25000, 5979, 'TAK', 51),
+(29, 'Lamborghini', 'Hurracan', 'Coupe', 2016, 'Benzyna', 34532, 5979, 'TAK', 51),
+(30, 'Lada', 'Samara', 'Hatchback', 1995, 'Benzyna', 350500, 59, 'TAK', 51),
+(31, 'Pagani', 'Zonda', 'Coupe', 2008, 'Benzyna', 5000, 9755, 'TAK', 51),
+(32, 'Honda', 'CRX', 'Hatchback', 1996, 'Benzyna', 255600, 125, 'TAK', 51),
+(33, 'Ferrari', 'Enzo', 'Coupe', 2008, 'Benzyna', 25000, 5979, 'TAK', 51),
+(34, 'Lamborghini', 'Hurracan', 'Coupe', 2016, 'Benzyna', 34532, 5979, 'TAK', 51),
+(35, 'Lada', 'Samara', 'Hatchback', 1995, 'Benzyna', 350500, 59, 'TAK', 51),
+(36, 'Pagani', 'Zonda', 'Coupe', 2008, 'Benzyna', 5000, 9755, 'TAK', 51),
+(37, 'Honda', 'Civic', 'Hatchback', 1996, 'Benzyna', 255600, 125, 'TAK', 51);
 
 -- --------------------------------------------------------
 
@@ -138,7 +153,17 @@ INSERT INTO `user` (`user_id`, `login`, `haslo`, `imie`, `nazwisko`, `data_urodz
 (56, 'tommy1234', 'tommy', 'Tomek', 'Chudy', '1996-02-20', 'Radymno', 567345890, 'chudy@gmail.com', '96022012333', 'worker'),
 (91, 'klient', 'klient', 'Janusz', 'Bach', '1989-07-07', 'Szczecin', 707963123, 'bach@gmail.com', '89070712345', 'klient'),
 (92, 'pracownik', 'pracownik', 'Mariusz', 'Strach', '1989-07-07', 'Szczecin', 707963434, 'strach@gmail.com', '89070754321', 'worker'),
-(93, 'admin', 'admin', 'Marian', 'Wolan', '1989-07-07', 'Radom', 704213434, 'wolan@gmail.com', '89070754344', 'admin');
+(93, 'admin', 'admin', 'Marian', 'Wolan', '1989-07-07', 'Radom', 704213434, 'wolan@gmail.com', '89070754344', 'admin'),
+(94, 'pracownik1000', 'pracownik1000', 'Ryszard', 'Nowak', '1991-05-20', 'Warszawa', 773050222, 'pracownik1000@email.com', '91052012345', 'worker'),
+(95, 'klient1000', 'klient1000', 'Piotr', 'Pawlikowski', '1992-03-26', 'Warszawa', 773050333, 'klient1000@email.com', '92032612345', 'worker'),
+(96, 'admin1000', 'admin1000', 'Piotr', 'Pawlikowski', '1984-08-11', 'Warszawa', 773050444, 'admin1000@email.com', '84081112345', 'worker'),
+(97, 'pracownik1000', 'pracownik1000', 'Ryszard', 'Nowak', '1991-05-20', 'Warszawa', 773050222, 'pracownik1000@email.com', '91052012345', 'worker'),
+(98, 'klient1000', 'klient1000', 'Piotr', 'Pawlikowski', '1992-03-26', 'Warszawa', 773050333, 'klient1000@email.com', '92032612345', 'worker'),
+(99, 'admin1000', 'admin1000', 'Piotr', 'Pawlikowski', '1984-08-11', 'Warszawa', 773050444, 'admin1000@email.com', '84081112345', 'worker'),
+(100, 'pracownik1000', 'pracownik1000', 'Ryszard', 'Nowak', '1991-05-20', 'Warszawa', 773050222, 'pracownik1000@email.com', '91052012345', 'worker'),
+(101, 'klient1000', 'klient1000', 'Piotr', 'Pawlikowski', '1992-03-26', 'Warszawa', 773050333, 'klient1000@email.com', '92032612345', 'worker'),
+(102, 'admin1001', 'admin1000', 'Piotr', 'Pawlikowski', '1984-08-11', 'Warszawa', 773050444, 'admin1000@email.com', '84081112345', 'worker'),
+(103, 'admin1002', 'admin1000', 'Piotr', 'Pawlikowski', '1984-08-11', 'Warszawa', 773050494, 'admin1088@email.com', '84081112999', 'worker');
 
 -- --------------------------------------------------------
 
@@ -164,7 +189,11 @@ INSERT INTO `wypozyczenie` (`wypozyczenie_id`, `user_id`, `samochod_id`, `data_o
 (6, 91, 2, '2019-05-06', '2019-05-18'),
 (7, 92, 6, '2019-05-01', '2019-05-04'),
 (8, 92, 7, '2019-05-20', '2019-05-27'),
-(9, 92, 9, '2019-05-23', '2019-05-25');
+(9, 92, 9, '2019-05-23', '2019-05-25'),
+(10, 92, 13, '2019-06-01', '2019-06-08'),
+(11, 92, 10, '2019-05-30', '2019-06-03'),
+(12, 91, 11, '2019-06-05', '2019-06-07'),
+(13, 91, 14, '2019-05-30', '2019-05-31');
 
 --
 -- Indeksy dla zrzutów tabel
@@ -212,7 +241,7 @@ ALTER TABLE `wypozyczenie`
 -- AUTO_INCREMENT dla tabeli `samochod`
 --
 ALTER TABLE `samochod`
-  MODIFY `samochod_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=84;
+  MODIFY `samochod_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 
 --
 -- AUTO_INCREMENT dla tabeli `udostepnienie`
@@ -224,13 +253,13 @@ ALTER TABLE `udostepnienie`
 -- AUTO_INCREMENT dla tabeli `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=94;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=106;
 
 --
 -- AUTO_INCREMENT dla tabeli `wypozyczenie`
 --
 ALTER TABLE `wypozyczenie`
-  MODIFY `wypozyczenie_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `wypozyczenie_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- Ograniczenia dla zrzutów tabel
